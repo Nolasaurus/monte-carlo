39a08d9070065302f9a7efefe747c2e6f2f89abe
Nolan Meyer <meyer09@gmail.com>
2022-11-26 00:25:14 -0600

    readme; skeleton for general MCM

---
+++ general monte carlo.py
@@ -0,0 +1,10 @@
+### calculate the pdf of ultimate results using random sampling for underlying paramaters
+### based on their own statistical probabilities
+
+##TODO accept inputs for parameters
+
+##TODO create model for outputing result
+
+##TODO iterate n times
+
+##TODO log results and graph output
